1f784881225e1e7f70c4ff9ac2d404e00be55187
Sacha <sachacollev@gmail.com>
2021-04-20 16:57:33 +0200

    push3

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function initVue(){
     data:{
      search:'',
      arrFilms:[],
+     arrSeries:[],
     },
     computed: {
       // filteredFilm: function(){
@@ -13,25 +14,46 @@ function initVue(){
       // }
     },
     methods:{
+      clickSearch: function(){
+        axios.get("https://api.themoviedb.org/3/search/movie", {
+          params:{
+           "api_key":"ddde7396f3d042a400eadf2474278e89",
+           "query": this.search,
+          }
+        })
+        .then(data => {
+          const films = data.data.results;
+          this.arrFilms = films;
+          console.log(this.arrFilms);
+
+        });
+        axios.get("https://api.themoviedb.org/3/search/tv", {
+          params:{
+           "api_key":"ddde7396f3d042a400eadf2474278e89",
+           "query": this.search,
+          }
+        })
+        .then(data => {
+          const serieTv = data.data.results;
+          this.arrSeries = serieTv;
+          console.log(this.arrSeries);
+
+        })
+        .catch(() => console.log('error'));
+      },
+      getFlag: function(language){
+        if (language == "it"){
+          return "img/it.png"
+        } else if (language == "en"){
+          return "img/en.png"
+        }
+      },
       // filteredFilm: function() {
       //     if (this.search == '') {
       //         return this.film;
       //     }
     },
     mounted() {
-      axios.get("https://api.themoviedb.org/3/search/movie", {
-        params:{
-         "api_key":"ddde7396f3d042a400eadf2474278e89",
-         "query": "matrix",
-        }
-      })
-      .then(data => {
-        const films = data.data.results;
-        this.arrFilms = films;
-        console.log(this.arrFilms);
-
-      })
-      .catch(() => console.log('error'));
     },
   });
 }
